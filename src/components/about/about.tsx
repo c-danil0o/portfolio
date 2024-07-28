67a30b6db8d7e612426ff7f6ae0fb6059e954330
Danilo <danilo.cvijetic10@protonmail.ch>
2024-07-28 20:34:06 +0200

    fix: host cv file on github

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -46,7 +46,7 @@ export default function About() {
       </div>
       <button className="p-[3px] relative mb-10">
         <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-violet-800 rounded-lg" />
-        <a download="CV_Danilo_Cvijetic.pdf" href="../../assets/CV.pdf">
+        <a download="CV_Danilo_Cvijetic.pdf" href="https://raw.githubusercontent.com/c-danil0o/portfolio/main/src/assets/CV.pdf">
           <div className="px-8 py-2  bg-black rounded-[6px]  relative group transition duration-200 text-neutral-300 hover:bg-transparent">
             Get my resume
           </div>
